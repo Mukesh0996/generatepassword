590abc3d298a9810b26dad68c286251db726509a
Mukesh0996 <34233598+Mukesh0996@users.noreply.github.com>
2023-08-22 13:02:37 +0530

    added char.js file

---
+++ utils/char.js
@@ -0,0 +1,4 @@
+export const lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+export const upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYX";
+export const numbers = "0123456789";
+export const splChars = "~`!@#$%^&*()_+{}[]||\\:;'',./?><";
