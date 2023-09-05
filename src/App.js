b5c22733494f26da48953d9fa52822a521dc81e8
Mukesh0996 <34233598+Mukesh0996@users.noreply.github.com>
2023-09-05 11:08:24 +0530

    added function to generate password

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
 import Form from './Components/FormC/Form';
 
-
-
 function App() {
+
 
   return (
     <div className="App">
       <Form/>
+
     </div>
   );
 }
--- src/Components/FormC/Form.js
+++ src/Components/FormC/Form.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import formStyle from './Form.module.css';
+import { lowerCaseCharacters, numbers, splChars, upperCaseCharacters } from '../../utils/char';
 
 const Form = () => {
 
@@ -7,7 +8,42 @@ const Form = () => {
     const [useLowerCase, setLowerCase] = useState(false);
     const [useNumericals, setNumericals] = useState(false);
     const [useSpecialChars, setSpecialChars] = useState(false);
+    const [generatedPassword, setGeneratedPassword] = useState("");
+    const [passwordLength, setPasswordLength] = useState(10);
 
+    const generatePasswordHandler = (e) => {
+            e.preventDefault();
+            // console.log("Executing");
+        if(!useUpperCase && !useLowerCase && !useNumericals && !useSpecialChars) {
+
+        } else {
+            let characterList = "";
+            if(useUpperCase) {
+                characterList+=upperCaseCharacters;
+            } 
+            if(useLowerCase) {
+                characterList+=lowerCaseCharacters;
+            }
+            if(useNumericals) {
+                characterList+= numbers;
+            }
+            if(useSpecialChars) {
+                characterList+=splChars;
+            }
+            createPassword(characterList);
+        }
+    };
+
+    const createPassword = (characterList) => {
+
+        let password = "";
+
+        for(let i =0; i < passwordLength; i++) {
+            const characterIndex = Math.round(Math.random() * characterList.length);
+            password += characterList.charAt(characterIndex);            
+        };
+        console.log("generated password", password);
+    };
 
     return <div className={formStyle.formDiv}>
                 <h3>Generate Your Password</h3>
@@ -16,7 +52,7 @@ const Form = () => {
                     <form className={formStyle.form}>
                         <div className={formStyle.controls}>
                             <div className={formStyle.checkbox_container}>
-                                <input id='ucase' type='checkbox' onChange={(e)=> setUpperCase(e.target.checked) } checked={useUpperCase}/>
+                                <input id='ucase' type='checkbox' onChange={(e)=> setUpperCase(e.target.checked)} checked={useUpperCase}/>
                                 <label htmlFor='ucase' className={formStyle.checkboxLabel}></label>
                             </div>
                             <span className={formStyle.labeltext}>Include Uppercase Characters</span>
@@ -42,8 +78,17 @@ const Form = () => {
                             </div>
                             <span className={formStyle.labeltext}>Include Special Characters</span>
                         </div>
+                        <div className={formStyle.form_btn_control} style={{marginBottom:'.5rem', textAlign:'center'}}>
+                            <input className={formStyle.passwordLength} type='number' step='1' value={passwordLength} min='8' max='15' onChange={(e) => setPasswordLength(e.target.value)}/>
+                            <label> 
+                                <span className={formStyle.labeltext}>Set Password Length</span>
+                            </label>
+                            <div>
+                                <span className={formStyle.spanalert}>* Minimum allowed password length is 8, and the maximum length is 15</span>
+                            </div>
+                        </div>
                         <div className={formStyle.form_btn_control}>
-                            <button type='submit'>Generate Password</button>
+                            <button type='submit' onClick={generatePasswordHandler}>Generate Password</button>
                         </div>
                     </form>
                 </div>
